17ec2b4514df71421ad36df051133989f8a62257
javiMax <coherentsheafjavi@proton.me>
2025-09-10 15:17:27 -0600

    removed katex imports from sheafs.tsx as it kept preventing build by deploy.yml on github

--- src/routes/geometry/sheafs.tsx
+++ src/routes/geometry/sheafs.tsx
@@ -1,5 +1,4 @@
-import {BlockMath, InlineMath} from 'react-katex';
-import 'katex/dist/katex.min.css';
+
 
 
 function SheafTheory(){
@@ -8,7 +7,6 @@ function SheafTheory(){
         <>
             The 3 primary resources for this section are Sheaf Theory by Tennison, Sheaves in Geometry & Logic by Maclane/Moerdijk and Categories & Sheaves by Kashiwara/Schapira.
 
-            <BlockMath> test</BlockMath>
         </>
     );
 
